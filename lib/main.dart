135b1de25377f9cea21fa2949940d69d65caf0d2
bekatadesse <bekatadesse5@gmail.com>
2024-03-08 17:38:33 +0300

    day 2 progress

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,74 @@ class AppCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(backgroundColor: Colors.grey[850],
       appBar: AppBar(
-        title: Text('AppCard'),
-      )
-    );
+        title: const Text('AppCard'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[800]
+        ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(vertical: 20,horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/pic-2.jpg'),
+                  radius: 80,
+                ),
+              ),
+            ),
+            Divider(
+              height: 120,
+              color: Colors.grey,
+            ),
+            Text('Name :',
+            style: TextStyle(
+              color: Colors.grey,
+              letterSpacing: 2.0,),),
+
+            Text('Venom-li',
+              style: TextStyle(
+                color: Colors.yellow,
+                letterSpacing: 2.0,
+                fontSize: 25,
+                fontWeight: FontWeight.bold),),
+            SizedBox(height: 20,),
+
+            Text('Current Level',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,),),
+
+            Text('10',
+              style: TextStyle(
+                  color: Colors.yellow,
+                  letterSpacing: 2.0,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),),
+            SizedBox(height: 20,),
+
+            Row(
+              children: [
+                Icon(
+                  Icons.mail,
+                  color: Colors.grey,),
+                SizedBox(width: 10,),
+                Text(
+                  'venom_li@code.co',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20,
+                    letterSpacing: 2),)
+              ],
+            )
+          ],
+        ),
+      ),
+  );
   }
 }
+
