600835554810272d29b040b667ac74551c2938a2
bekatadesse <bekatadesse5@gmail.com>
2024-03-10 22:16:08 +0300

    day 5 progress fixed a bug

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class AppCard extends StatefulWidget {
 class _AppCardState extends State<AppCard> {
   // data that change over time
   //int ninjaLevel = 0;
-  int _ninjaLevel = 0; // Example mutable state
+  int ninjaLevel = 0; // Example mutable state
 
   @override
   Widget build(BuildContext context) {
@@ -28,19 +28,19 @@ class _AppCardState extends State<AppCard> {
       floatingActionButton: FloatingActionButton(
         onPressed: (){
           setState(() {
-            _ninjaLevel +=1;
+            ninjaLevel +=1;
           });
 
         },
         backgroundColor: Colors.grey[800],
         child: const Icon(Icons.add),
       ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(vertical: 20,horizontal: 30),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20,horizontal: 30),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Center(
+            const Center(
               child: Padding(
                 padding: EdgeInsets.all(20.0),
                 child: CircleAvatar(
@@ -49,38 +49,38 @@ class _AppCardState extends State<AppCard> {
                 ),
               ),
             ),
-            Divider(
+            const Divider(
               height: 120,
               color: Colors.grey,
             ),
-            Text('Name :',
+            const Text('Name :',
             style: TextStyle(
               color: Colors.grey,
               letterSpacing: 2.0,),),
 
-            Text('Venom-li',
+            const Text('Venom-li',
               style: TextStyle(
                 color: Colors.yellow,
                 letterSpacing: 2.0,
                 fontSize: 25,
                 fontWeight: FontWeight.bold),),
-            SizedBox(height: 20,),
+            const SizedBox(height: 20,),
 
-            Text('Current Level',
+            const Text('Current Level',
               style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 2.0,),),
 
             Text(
-              '$_ninjaLevzel',
-              style: TextStyle(
+              '$ninjaLevel',
+              style: const TextStyle(
                   color: Colors.yellow,
                   letterSpacing: 2.0,
                   fontSize: 25,
                   fontWeight: FontWeight.bold),),
-            SizedBox(height: 20,),
+            const SizedBox(height: 20,),
 
-            Row(
+            const Row(
               children: [
                 Icon(
                   Icons.mail,
